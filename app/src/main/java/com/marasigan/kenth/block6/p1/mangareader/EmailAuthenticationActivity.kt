f9893a1d9acbf10c9ee77ffdd259370a5fad0170
Vern_Pherus <145563323+VernPherus@users.noreply.github.com>
2025-03-28 20:43:34 +0800

    added email activity

---
+++ app/src/main/java/com/marasigan/kenth/block6/p1/mangareader/EmailAuthenticationActivity.kt
@@ -0,0 +1,20 @@
+package com.marasigan.kenth.block6.p1.mangareader
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class EmailAuthenticationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_email_authentication)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
